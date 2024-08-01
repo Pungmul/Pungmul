8ba3c8ccaea14601a63e7ee127d579f6b4d94af7
kyh885 <kyh885@gmail.com>
2024-08-01 15:38:52 +0900

    init

---
+++ src/main/java/pungmul/pungmul/PungmulApplication.java
@@ -0,0 +1,13 @@
+package pungmul.pungmul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PungmulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PungmulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pungmul/pungmul/PungmulApplicationTests.java
@@ -0,0 +1,13 @@
+package pungmul.pungmul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PungmulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
