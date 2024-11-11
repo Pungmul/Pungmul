1701535ebfe5d8c5d0eb218f68191c7ca3159214
kyh885 <kyh885@gmail.com>
2024-11-12 03:39:31 +0900

    #30 https 로컬 설정

--- src/main/java/pungmul/pungmul/PungmulApplication.java
+++ src/main/java/pungmul/pungmul/PungmulApplication.java
@@ -1,7 +1,17 @@
 package pungmul.pungmul;
 
+import org.apache.catalina.webresources.StandardRoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+
 
 @SpringBootApplication
 public class PungmulApplication {
@@ -10,4 +20,27 @@ public class PungmulApplication {
 		SpringApplication.run(PungmulApplication.class, args);
 	}
 
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				// Context 내의 보안 설정 적용
+				context.setResources(new StandardRoot(context));
+			}
+		};
+
+		// HTTP -> HTTPS 리다이렉트 설정
+		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+		return tomcat;
+	}
+
+	private Connector httpToHttpsRedirectConnector() {
+		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(443);
+		return connector;
+	}
 }
--- src/main/java/pungmul/pungmul/config/S3Config.java
+++ src/main/java/pungmul/pungmul/config/S3Config.java
@@ -1,5 +1,6 @@
 package pungmul.pungmul.config;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +8,11 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Configuration
 public class S3Config {
@@ -20,6 +26,14 @@ public class S3Config {
     @Value("${cloud.aws.region.static}")
     private String region;
 
+    @Value("${aws.s3.bucket-name}")
+    private String bucketName;
+
+    @Value("${aws.s3.key-path}")
+    private String keyPath;
+
+//    private final Path keystorePath = Paths.get(System.getProperty("java.io.tmpdir"), "keystore.p12");
+
     @Bean
     public S3Client s3Client() {
         AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(
@@ -28,8 +42,33 @@ public class S3Config {
         );
 
         return S3Client.builder()
-                .region(Region.of(region))  // AWS 리전을 설정
+                .region(Region.of(region))  // AWS 리전 설정
                 .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
                 .build();
     }
+
+//    @PostConstruct
+//    public void downloadKeystore() {
+//        try {
+//            if (Files.notExists(keystorePath)) {
+//                // S3에서 인증서를 다운로드
+//                s3Client().getObject(
+//                        GetObjectRequest.builder()
+//                                .bucket(bucketName)
+//                                .key(keyPath)
+//                                .build(),
+//                        keystorePath
+//                );
+//                System.out.println("Keystore downloaded to: " + keystorePath.toString());
+//            }
+//        } catch (Exception e) {
+//            System.err.println("Failed to download keystore from S3: " + e.getMessage());
+//            e.printStackTrace();
+//        }
+//    }
+
+//    @Bean
+//    public Path keystorePath() {
+//        return keystorePath;
+//    }
 }
