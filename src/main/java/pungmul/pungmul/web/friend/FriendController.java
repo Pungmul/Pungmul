534a2320c49c9e6799eb71fb65b9195e5ff4b03e
yjs <yjs9714@gmail.com>
2025-03-11 01:32:19 +0900

    feat: swagger 추가/friend 완료

--- src/main/java/pungmul/pungmul/web/friend/FriendController.java
+++ src/main/java/pungmul/pungmul/web/friend/FriendController.java
@@ -1,5 +1,9 @@
 package pungmul.pungmul.web.friend;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -26,6 +30,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@Tag(name= "friends", description = "친구 관련 API 모음입니다.")
 @RequestMapping("/api/friends")
 public class FriendController {
     private final FriendService friendService;
@@ -34,6 +39,8 @@ public class FriendController {
     //  친구 목록 보기
     @PreAuthorize("hasRole('USER')")
     @GetMapping("")
+    @Operation(summary = "친구 목록 보기", description = "친구 목록을 조회한다.")
+    @ApiResponse(responseCode = "2000", description = "성공")
     public ResponseEntity<BaseResponse<FriendListResponseDTO>> getFriendList(
             @AuthenticationPrincipal UserDetails userDetails){
         FriendListResponseDTO friendList = friendService.getFriendList(userDetails);
@@ -43,6 +50,8 @@ public class FriendController {
     //  친구 요청 가능한 사용자 목록
     @PreAuthorize("hasRole('USER')")
     @GetMapping("/request")
+    @Operation(summary = "친구 요청 가능한 사용자 목록", description = "친구 요청 가능한 사용자 목록을 조회한다.")
+    @ApiResponse(responseCode = "2000", description = "성공")
     public ResponseEntity<BaseResponse<List<AvailableFriendDTO>>> searchUsers(
             @AuthenticationPrincipal UserDetails userDetails,
             @RequestParam(required = false) String keyword) {
@@ -53,8 +62,12 @@ public class FriendController {
     //  친구 신청
     @PreAuthorize("hasRole('USER')")
     @PostMapping("/request")
+    @Operation(summary = "친구 신청", description = "친구 신청을 한다.")
+    @ApiResponse(responseCode = "2000", description = "성공")
     public ResponseEntity<BaseResponse<FriendReqResponseDTO>> sendFriendRequest(
+            @Parameter(description = "현재 인증된 사용자 정보", hidden = true)
             @AuthenticationPrincipal UserDetails userDetails,
+            @Parameter(description = "친구 요청을 받을 사람의 이름", required = true)
             @RequestParam String receiverUsername
             ){
         FriendReqResponseDTO friendReqResponseDTO = friendService.sendFriendRequest(userDetails, receiverUsername);
@@ -76,7 +89,10 @@ public class FriendController {
     //  친구 요청 수락 -> 친구 목록 창에서 특정 사용자에게 와있는 pending 상태의 친구 관계를 ACCEPTED로 변경
     @PreAuthorize("hasRole('USER')")
     @PostMapping("/accept")
+    @Operation(summary = "친구 요청 수락", description = "친구 요청을 수락한다.")
+    @ApiResponse(responseCode = "2002", description = "요청이 성공적으로 접수되었습니다.")
     public ResponseEntity<BaseResponse<Void>> acceptFriendRequest(
+            @Parameter(description = "요청온 친구의 ID", required = true)
             @RequestParam Long friendRequestId
     ){
         friendService.acceptFriendRequest(friendRequestId);
@@ -86,7 +102,10 @@ public class FriendController {
     //  친구 요청 거절 -> 친구 목록 창에서 특정 사용자에게 와있는 pending 상태의 친구 관계를 DECLINED으로 변경
     @PreAuthorize("hasRole('USER')")
     @PostMapping("/decline")
+    @Operation(summary = "친구 요청 거절", description = "친구 요청을 거절한다.")
+    @ApiResponse(responseCode = "2002", description = "요청이 성공적으로 접수되었습니다.")
     public ResponseEntity<BaseResponse<Void>> declineFriendRequest(
+            @Parameter(description = "요청온 친구의 ID", required = true)
         Long friendRequestId){
         friendService.declineFriendRequest(friendRequestId);
         return ResponseEntity.ok(BaseResponse.ofSuccess(BaseResponseCode.ACCEPTED));
@@ -95,7 +114,10 @@ public class FriendController {
     //  차단 -> 친구 관계를 BLOCK으로 변경
     @PreAuthorize("hasRole('USER')")
     @PostMapping("/block")
+    @Operation(summary = "친구 차단", description = "친구를 차단한다")
+    @ApiResponse(responseCode = "2002", description = "요청이 성공적으로 접수되었습니다.")
     public ResponseEntity<BaseResponse<Void>> blockFriend(
+            @Parameter(description = "차단할 친구의 ID", required = true)
             Long friendRequestId){
         friendService.blockFriend(friendRequestId);
         return ResponseEntity.ok(BaseResponse.ofSuccess(BaseResponseCode.ACCEPTED));
